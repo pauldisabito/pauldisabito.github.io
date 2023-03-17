d793717be8121b5932d3434d71a17b58ec329be3
Paul DiSabito <pdisabito@salesforce.com>
2023-03-17 17:00:07 -0400

    Bunch of changes to get search functionality.

---
+++ assets/js/search.js
@@ -0,0 +1,11 @@
+---
+---
+window.onload = function() {
+  SimpleJekyllSearch({
+    searchInput: document.getElementById('search-input'),
+    resultsContainer: document.getElementById('results-container'),
+    json: '{{ "/search.json" | relative_url }}',
+    searchResultTemplate: '<li><a href="{url}">{title}</a></li>',
+    noResultsText: 'No results found',
+  });
+}
