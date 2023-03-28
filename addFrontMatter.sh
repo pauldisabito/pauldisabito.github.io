30315ce1f140d179ed6ecef12e764d5862a27a64
Paul DiSabito <pdisabito@salesforce.com>
2023-03-28 13:40:35 -0400

    Splitting content into 'current' and 'upcoming'

--- addFrontMatter.sh
+++ addFrontMatter.sh
@@ -5,12 +5,21 @@
 # Before you start, however, rename the following files, because we don't want
 # them to have Jekyll front matter.
 
-mv docs/scriptapi/html/index.html docs/scriptapi/html/index.html.new
-mv docs/pipeletapi/html/index.html docs/pipeletapi/html/index.html.new
-mv docs/compareapi/html/index.html docs/compareapi/html/index.html.new
-mv docs/jobstepapi/html/index.html docs/jobstepapi/html/index.html.new
-mv docs/xsd/index.html docs/xsd/index.html.new
-mv docs/content/index.html docs/content/index.html.new
+# Current release
+mv docs/current/scriptapi/html/index.html docs/current/scriptapi/html/index.html.new
+mv docs/current/pipeletapi/html/index.html docs/current/pipeletapi/html/index.html.new
+mv docs/current/compareapi/html/index.html docs/current/compareapi/html/index.html.new
+mv docs/current/jobstepapi/html/index.html docs/current/jobstepapi/html/index.html.new
+mv docs/current/xsd/index.html docs/current/xsd/index.html.new
+mv docs/current/content/index.html docs/current/content/index.html.new
+
+# Upcoming release
+mv docs/upcoming/scriptapi/html/index.html docs/upcoming/scriptapi/html/index.html.new
+mv docs/upcoming/pipeletapi/html/index.html docs/upcoming/pipeletapi/html/index.html.new
+mv docs/upcoming/compareapi/html/index.html docs/upcoming/compareapi/html/index.html.new
+mv docs/upcoming/jobstepapi/html/index.html docs/upcoming/jobstepapi/html/index.html.new
+mv docs/upcoming/xsd/index.html docs/upcoming/xsd/index.html.new
+mv docs/upcoming/content/index.html docs/upcoming/content/index.html.new
 
 files=$(find docs -type f -name "*.html")
 
@@ -32,9 +41,31 @@ for file in $files; do
   rm "$file.new"
 done
 
+# Now that the front matter was added to all of the html files, 
+# move the ".new" files to effectively strip off the file extension 
+
 mv docs/scriptapi/html/index.html.new docs/scriptapi/html/index.html
 mv docs/pipeletapi/html/index.html.new docs/pipeletapi/html/index.html
 mv docs/compareapi/html/index.html.new docs/compareapi/html/index.html
 mv docs/jobstepapi/html/index.html.new docs/jobstepapi/html/index.html
 mv docs/xsd/index.html.new docs/xsd/index.html
 mv docs/content/index.html.new  docs/content/index.html
+mv docs/upcoming/scriptapi/html/index.html.new docs/upcoming/scriptapi/html/index.html
+mv docs/upcoming/pipeletapi/html/index.html.new docs/upcoming/pipeletapi/html/index.html
+mv docs/upcoming/compareapi/html/index.html.new docs/upcoming/compareapi/html/index.html
+mv docs/upcoming/jobstepapi/html/index.html.new docs/upcoming/jobstepapi/html/index.html
+mv docs/upcoming/xsd/index.html.new docs/upcoming/xsd/index.html
+mv docs/upcoming/content/index.html.new  docs/upcoming/content/index.html
+
+# TODO: For a set of specific files, replace "layout: default" with "layout: default_no_home"
+
+#docs/compareapi/html/whatsnew/toc.html
+#docs/current/compareapi/html/whatsnew/toc.html
+#docs/upcoming/compareapi/html/whatsnew/toc.html
+
+# TODO: Fix title problem in jekyll front matter with single quotes 
+
+# TODO: Strip all instances of dwscriptlarge.gif.
+
+# TODO: Strip all copyright stuff from ecom files. For example:
+# © Copyright 2000-2023, salesforce.com inc. All rights reserved. Various trademarks held by their respective owners.
