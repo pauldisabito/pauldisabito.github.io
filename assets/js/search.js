9564e6cc74143b3c41693b36f86a8b4563516ff9
Paul DiSabito <pdisabito@salesforce.com>
2023-06-22 09:59:00 -0400

    Further changes to search.js.

--- assets/js/search.js
+++ assets/js/search.js
@@ -41,39 +41,46 @@ document.addEventListener("DOMContentLoaded", function() {
     console.log("Search Query:", query); // Print the search query
 
     // Perform the search using Lunr.js
-    var results = index.search(function(query) {
-      query.term(query.term, {
-        fields: ["title", "category", "tags", "content"],
-        boost: 10
+    try {
+      var searchTerms = query; // Store the search terms separately
+
+      var results = index.search(function(query) {
+        query.term(searchTerms, { // Use the separate search terms
+          fields: ["title", "category", "tags", "content"],
+          boost: 10
+        });
+        query.term(searchTerms, { // Use the separate search terms
+          fields: ["title", "category", "tags", "content"]
+        });
       });
-      query.term(query.term, {
-        fields: ["title", "category", "tags", "content"]
-      });
-    });
 
-    // Display the search results
-    searchResults.innerHTML = ""; // Clear previous results
+      // Display the search results
+      searchResults.innerHTML = ""; // Clear previous results
 
-    if (results.length > 0) {
-      // Loop through the search results and display them
-      for (var i = 0; i < results.length; i++) {
-        var result = results[i];
-        var page = result.ref;
-        var title = result.doc.title;
+      if (results.length > 0) {
+        // Loop through the search results and display them
+        for (var i = 0; i < results.length; i++) {
+          var result = results[i];
+          var page = result.ref;
+          var title = result.doc.title;
 
-        // Create a link to the search result page
-        var link = document.createElement("a");
-        link.href = page;
-        link.textContent = title;
+          // Create a link to the search result page
+          var link = document.createElement("a");
+          link.href = page;
+          link.textContent = title;
 
-        // Append the link to the search results element
-        searchResults.appendChild(link);
+          // Append the link to the search results element
+          searchResults.appendChild(link);
+        }
+      } else {
+        // Display a message if no results were found
+        var message = document.createElement("p");
+        message.textContent = "No results found.";
+        searchResults.appendChild(message);
       }
-    } else {
-      // Display a message if no results were found
-      var message = document.createElement("p");
-      message.textContent = "No results found.";
-      searchResults.appendChild(message);
+    } catch (error) {
+      console.log("Error performing search:", error);
+      // Display an error message to the user or handle the error in another way
     }
   });
 });
