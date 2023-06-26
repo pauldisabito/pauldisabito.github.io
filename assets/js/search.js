c61cf16d5057cb3b1690d36f8c9876bd4f938792
Paul DiSabito <pdisabito@salesforce.com>
2023-06-26 09:34:31 -0400

    Further changes to search.js to correctly use idx variable.

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,88 +1,75 @@
 // Wait for the DOM to be ready
 document.addEventListener("DOMContentLoaded", function() {
-    // Initialize Lunr.js and create a search index
-    var index = lunr(function() {
-        this.field("title");
-        this.field("content");
-        this.ref("url");
+  // Initialize Lunr.js and create a search index
+  var idx;
+  lunr(function() {
+    this.field('title');
+    this.field('content');
+    this.ref('url');
+    idx = this; // Assign the index to the outer variable
 
-        var lunrIndex = this; // Reference to the Lunr index object
+    // Fetch the search data from the search.json file
+    fetch("../../search.json")
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        // Add the data to the search index
+        data.forEach(function(page) {
+          if (Object.keys(page).length > 0) {
+            idx.add(page);
+          }
+        });
+      })
+      .catch(function(error) {
+        console.log("Error fetching search data:", error);
+      });
+  });
 
-        // Fetch the search data from the search.json file
-        fetch("../../search.json")
-            .then(function(response) {
-                return response.json();
-            })
-            .then(function(data) {
-                // Add the data to the search index
-                data.forEach(function(page) {
-                    if (Object.keys(page).length > 0) {
-                        lunrIndex.add(page);
-                        // console.log("Indexed page:", page); // Log the indexed page object
-                    }
-                });
+  // Perform the search when the user submits the form
+  document.getElementById("search-form").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent form submission
 
-                console.log("Here is the populated index:", index);
-            })
-            .catch(function(error) {
-                console.log("Error fetching search data:", error);
-            });
-    });
-});
-
-
-
-
-
-
-
-    // Get references to the search input and search results elements
     var searchInput = document.getElementById("search-input");
     var searchResults = document.getElementById("search-results");
 
-    // Perform the search when the user submits the form
-    document.getElementById("search-form").addEventListener("submit", function(event) {
-        event.preventDefault(); // Prevent form submission
+    var searchQuery = searchInput.value.trim(); // Get the search query
 
-        var searchquery = searchInput.value.trim(); // Get the search query
+    console.log("Search Query:", searchQuery); // Print the search query
 
-        console.log("Search Query:", searchquery); // Print the search query
+    // Check if the index is populated before performing the search
+    if (idx) {
+      // Perform the search using Lunr.js
+      var results = idx.search(searchQuery);
 
-        // Perform the search using Lunr.js
-        try {
-            var results = index.search(searchquery);
+      console.log("Here are the search results:", results);
 
-            console.log("Here is the index:", index);
+      // Display the search results
+      searchResults.innerHTML = ""; // Clear previous results
 
-            console.log("Here are the search results:", results);
+      if (results.length > 0) {
+        // Loop through the search results and display them
+        for (var i = 0; i < results.length; i++) {
+          var result = results[i];
+          var resultPage = result.ref;
+          var resultTitle = result.doc.title;
 
-            // Display the search results
-            searchResults.innerHTML = ""; // Clear previous results
+          // Create a link to the search result page
+          var link = document.createElement("a");
+          link.href = resultPage;
+          link.textContent = resultTitle;
 
-            if (results.length > 0) {
-                // Loop through the search results and display them
-                for (var i = 0; i < results.length; i++) {
-                    var result = results[i];
-                    var resultpage = result.ref;
-                    var resulttitle = result.doc.title;
-
-                    // Create a link to the search result page
-                    var link = document.createElement("a");
-                    link.href = resultpage;
-                    link.textContent = resulttitle;
-
-                    // Append the link to the search results element
-                    searchResults.appendChild(link);
-                }
-            } else {
-                // Display a message if no results were found
-                var message = document.createElement("p");
-                message.textContent = "No results found.";
-                searchResults.appendChild(message);
-            }
-        } catch (error) {
-            console.log("Error performing search:", error);
-            // Display an error message to the user or handle the error in another way
+          // Append the link to the search results element
+          searchResults.appendChild(link);
         }
-    });
+      } else {
+        // Display a message if no results were found
+        var message = document.createElement("p");
+        message.textContent = "No results found.";
+        searchResults.appendChild(message);
+      }
+    } else {
+      console.log("Index is not ready yet. Please wait and try again.");
+    }
+  });
 });
